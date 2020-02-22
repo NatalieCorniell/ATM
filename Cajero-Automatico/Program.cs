2395eec1a1ea8cc478f0344ea3c47470a52da22f
Natalie Georgia Corniell Rojas <nataliecorniell@MacBook-Pro-de-Natalie.local>
2020-02-22 18:27:39 -0400

    First Commit

---
+++ Cajero-Automatico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CajeroAutomatico
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
